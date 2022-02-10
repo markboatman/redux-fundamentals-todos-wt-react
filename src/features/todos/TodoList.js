5c1e31cbfe75b424fd7331c255109b364ed4abc2
Mark Boatman <markjboat@yahoo.com>
2022-02-10 22:43:33 +0800

    Status and color filters are working

--- src/features/todos/TodoList.js
+++ src/features/todos/TodoList.js
@@ -2,67 +2,54 @@ import React from 'react'
 import { useSelector, shallowEqual } from 'react-redux'
 import TodoListItem from './TodoListItem'
 
-const selectTodoIds = (state) => state.todos.map((todo) => todo.id)
-
-const selectTodoIdsFilterStatus = (state) => {
-  console.log('In selectTodoIdsFilterStatus, todos are: ', state.todos)
-  console.log(
-    'In selectTodoIdsFilterStatus, state.filters.status is : ',
-    state.filters.status
-  )
-  if (state.filters.status !== 'all') {
+const getTodosFilterOnStatus = (arTodos, statusFilter) => {
+  console.log('In selectTodosFilterOnStatus, todos are: ', arTodos)
+  console.log('In selectTodosFilterOnStatus, statusFilter is : ', statusFilter)
+  if (statusFilter !== 'all') {
     let filteredTodos
-    if (state.filters.status === 'active') {
-      filteredTodos = state.todos.filter((todo) => todo.completed === false)
-      return filteredTodos.map((todo) => todo.id)
-    } else if (state.filters.status === 'completed') {
-      filteredTodos = state.todos.filter((todo) => todo.completed === true)
-      return filteredTodos.map((todo) => todo.id)
+    if (statusFilter === 'active') {
+      filteredTodos = arTodos.filter((todo) => todo.completed === false)
+      return filteredTodos
+    } else if (statusFilter === 'completed') {
+      filteredTodos = arTodos.filter((todo) => todo.completed === true)
+      return filteredTodos
     }
   }
-  // else no status filter return all
-  return state.todos.map((todo) => todo.id)
-  // return filteredTodos.map((todo) => todo.id)
-  // state.todos.map((todo) => todo.id)
+  return arTodos
 }
 
-const selectTodoIdsFilterColor = (state) => {
-  console.log('In selectTodoIdsFilterColor, todos are: ', state.todos)
+const getTodosFilterOnColor = (arTodos, arColorFilters) => {
+  console.log('In selectTodosFilterOnColor, arTodos is: ', arTodos)
   console.log(
-    'In selectTodoIdsFilterColor, state.filters.colors are : ',
-    state.filters.colors
+    'In selectTodosFilterOnColor, arColorFilters is: ',
+    arColorFilters
   )
   let filteredTodos
-  if (state.filters.colors.length !== 0) {
-    filteredTodos = state.todos.filter((todo) => {
+  if (arColorFilters.length !== 0) {
+    filteredTodos = arTodos.filter((todo) => {
       if (todo.color) {
         // return truthy
-        return state.filters.colors.find((color) => color === todo.color)
+        return arColorFilters.find((color) => color === todo.color)
       } else {
         return false
       }
     })
-    return filteredTodos.map((todo) => todo.id)
+    return filteredTodos
   }
-  // ELSE
-  // filteredTodos = state.todos.map((todo) => {
-  //   console.log(
-  //     'In selectTodoIdsFilterColor if, state.filters.colors are : ',
-  //     state.filters.colors
-  //   )
-  //   if (state.filters.colors.find(todo.color)) {
-  //     return todo
-  //   }
-  // })
-  // just get the ids
-  return state.todos.map((todo) => todo.id)
+  return arTodos
+}
+
+const selectFilteredTodoIds = (state) => {
+  let arFilteredTodos = getTodosFilterOnStatus(
+    state.todos,
+    state.filters.status
+  )
+  arFilteredTodos = getTodosFilterOnColor(arFilteredTodos, state.filters.colors)
+  return arFilteredTodos.map((todo) => todo.id)
 }
 
 const TodoList = () => {
-  //const filteredTodos = useSelector(selectTodoIdsFilterStatus)
-  //console.log('In TodoList, filtered on status todos are: ', filteredTodos)
-  // TODO need to apply status filter here
-  const todoIds = useSelector(selectTodoIdsFilterColor, shallowEqual)
+  const todoIds = useSelector(selectFilteredTodoIds, shallowEqual)
   const renderedListItems = todoIds.map((todoId) => {
     return <TodoListItem key={todoId} id={todoId} />
   })
