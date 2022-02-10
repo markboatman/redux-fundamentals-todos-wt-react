d662a02f696e66769545ccf439b07fad6d25472f
Mark Boatman <markjboat@yahoo.com>
2022-02-10 11:19:32 +0800

    Test selector method selectTodoIdsNew in TodoList is working

--- src/features/todos/TodoList.js
+++ src/features/todos/TodoList.js
@@ -4,14 +4,36 @@ import TodoListItem from './TodoListItem'
 
 const selectTodoIds = (state) => state.todos.map((todo) => todo.id)
 
-const TodoList = () => {
-  const todoIds = useSelector(selectTodoIds, shallowEqual)
+const selectTodoIdsNew = (state) => {
+  console.log('In selectTodoIdsNew, todos are: ', state.todos)
+  console.log(
+    'In selectTodoIdsNew, state.filters.status is : ',
+    state.filters.status
+  )
+  if (state.filters.status !== 'all') {
+    let filteredTodos
+    if (state.filters.status === 'active') {
+      filteredTodos = state.todos.filter((todo) => todo.completed === false)
+      return filteredTodos
+    } else if (state.filters.status === 'completed') {
+      filteredTodos = state.todos.filter((todo) => todo.completed === true)
+      return filteredTodos
+    }
+  }
+  // else no status filter return all
+  return state.todos
+  // return filteredTodos.map((todo) => todo.id)
+  // state.todos.map((todo) => todo.id)
+}
 
+const TodoList = () => {
+  const filteredTodos = useSelector(selectTodoIdsNew)
+  console.log('In TodoList, filtered todos are: ', filteredTodos)
+  // TODO need to apply status filter here
+  const todoIds = useSelector(selectTodoIds, shallowEqual)
   const renderedListItems = todoIds.map((todoId) => {
     return <TodoListItem key={todoId} id={todoId} />
   })
-
-  console.log('In TodoList, num of todos is no: ', todoIds.length)
 
   return <ul className="todo-list">{renderedListItems}</ul>
 }
